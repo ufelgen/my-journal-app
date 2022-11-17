3273da963e1ab88fb46020d6df33e211c05c6ebb
Ulrike Felgenhauer <felgenhauer.u@gmail.com>
2022-11-17 17:26:44 +0100

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return <></>;
 }
 
-export default App;
+// importiert Header, main und footer
+// main importiert dann alles was in Main ist
+// verschachteln, erstmal aber ohne children props
+// Komponenten dort importieren, wo sie auch gebraucht werden.
---
+++ src/components/EntriesSection/EntriesSection.js
@@ -0,0 +1,16 @@
+import "./Entries.css";
+
+import Badge from "./Badge";
+import EntryList from "./EntryList";
+import Entry from "./Entry";
+import TabBar from "./TabBar";
+
+export function Title() {}
+
+export function Date() {}
+
+export function Text() {}
+
+export function FavIcon() {}
+
+export default function EntriesSection() {}
